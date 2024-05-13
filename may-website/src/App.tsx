bbe8d8691ea96c675507c16304f14b8629c7b900
BRIANMAY123 <1203857108@qq.com>
2024-05-13 17:35:11 +0800

    初步完成导航栏

---
+++ may-website/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Header } from './components/header/index';
+function App() {
+
+
+  return (
+    <div className=' w-full'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
